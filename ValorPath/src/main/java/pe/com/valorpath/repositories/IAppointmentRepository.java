4b39a0ab7cab849c01bb5a1e9efdc69de1776e3b
Jhulisa Rodas Burga <117694480+JhulisaRB@users.noreply.github.com>
2024-09-03 10:41:01 -0500

    Comit sin controller
    casi listo

---
+++ ValorPath/src/main/java/pe/com/valorpath/repositories/IAppointmentRepository.java
@@ -0,0 +1,8 @@
+package pe.com.valorpath.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.com.valorpath.entities.Appointment;
+@Repository
+public interface IAppointmentRepository extends JpaRepository<Appointment, Integer> {
+}
---
+++ ValorPath/src/main/java/pe/com/valorpath/serviceinterfaces/IAppointmentService.java
@@ -0,0 +1,13 @@
+package pe.com.valorpath.serviceinterfaces;
+
+import pe.com.valorpath.entities.Appointment;
+
+import java.util.List;
+
+public interface IAppointmentService {
+    public void insert (Appointment appointment);
+    public void update (Appointment appointment);
+    public void delete (int id);
+    public List<Appointment> list();
+    public Appointment listId (int id);
+}
