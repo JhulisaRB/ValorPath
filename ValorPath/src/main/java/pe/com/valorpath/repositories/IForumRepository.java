9d36d83be49b4a9e3b6c2e7f65f4e0e3829ee63c
Choxclin <108918759+Choxclin@users.noreply.github.com>
2024-09-10 20:47:40 -0500

    Creación de querys(n=5)

--- ValorPath/src/main/java/pe/com/valorpath/repositories/IForumRepository.java
+++ ValorPath/src/main/java/pe/com/valorpath/repositories/IForumRepository.java
@@ -1,12 +1,47 @@
 package pe.com.valorpath.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.com.valorpath.entities.Forum;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface IForumRepository extends JpaRepository<Forum, Integer> {
-    public List<Forum> ListarForosUsuarioV();
+
+
+    @Query("SELECT f FROM Forum f JOIN f.idUsers u WHERE f.idforum = :idforum")
+    public Forum buscarPorId(@Param("idforum") Integer idforum);
+
+
+    @Query("SELECT f FROM Forum f JOIN f.idUsers u WHERE f.idUsers.idUsers = :id_users")
+    public List<Forum> listarForosPorUsuario(@Param("id_users") Integer idUsers);
+
+    @Query(value = "SELECT COUNT(f.idforum) " +
+            "FROM Forum f " +
+            "JOIN Users u ON f.idUsers = u.idUsers " +
+            "WHERE f.date = :date", nativeQuery = true)
+    public Forum contarForosPorDia(@Param("date") LocalDate date);
+
+    @Query(value = "SELECT u.name, COUNT(f.idforum) " +
+            "FROM Forum f " +
+            "JOIN Users u ON f.idUsers = u.idUsers " +
+            "WHERE f.date = :date " +
+            "GROUP BY u.name", nativeQuery = true)
+    public List<Object[]> ListarForosPorDiaYUsuario(@Param("date") LocalDate date);
+
+    @Query("SELECT u.name, COUNT(f.idforum) " +
+            "FROM Forum f JOIN f.idUsers u " +
+            "WHERE f.idUsers.idUsers = :id_users " +
+            "GROUP BY u.name")
+    public List<Object[]> contarForosPorUsuario(@Param("id_users") Integer idUsers);
+
+
+
+
+
+
 }
